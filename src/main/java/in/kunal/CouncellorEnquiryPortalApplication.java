1b3400fcd028deb195d3bb87e0761bbcae88d421
Kunal <virajprajapat854@gmail.com>
2025-03-25 18:21:47 +0530

    intial commit

---
+++ src/main/java/in/kunal/CouncellorEnquiryPortalApplication.java
@@ -0,0 +1,13 @@
+package in.kunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouncellorEnquiryPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouncellorEnquiryPortalApplication.class, args);
+	}
+
+}
